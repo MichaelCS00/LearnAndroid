03a13bd15b31ac2654d654b589edee401cd552e6
Michael_liu <junhong@turingpic.com>
2018-08-09 08:01:16 +0800

    change the size of menu buttom

--- MaterialTest/app/src/main/java/com/example/michaelcs/materialtest/MainActivity.java
+++ MaterialTest/app/src/main/java/com/example/michaelcs/materialtest/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.michaelcs.materialtest;
 
 import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
@@ -10,6 +12,8 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 
@@ -24,7 +28,7 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        mDrawerLayout = findViewById(R.id.drawer_layout);
         ActionBar actionBar = getSupportActionBar();
         if(actionBar != null){
             actionBar.setDisplayHomeAsUpEnabled(true);
@@ -37,6 +41,23 @@ public class MainActivity extends AppCompatActivity
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 mDrawerLayout.closeDrawers();
                 return true;
+            }
+        });
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Snackbar.make(v,"Data deleted",Snackbar.LENGTH_SHORT)
+                        .setAction("Undo",new View.OnClickListener(){
+                            @Override
+                            public void onClick(View v)
+                            {
+                                Toast.makeText(MainActivity.this,"Data restored",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        }).show();
             }
         });
     }
@@ -52,6 +73,7 @@ public class MainActivity extends AppCompatActivity
         switch (item.getItemId()){
             case android.R.id.home:
                 mDrawerLayout.openDrawer(GravityCompat.START);
+                break;
             case R.id.backup:
                 Toast.makeText(this,"You clicked Backup",Toast.LENGTH_SHORT).show();
                 break;
