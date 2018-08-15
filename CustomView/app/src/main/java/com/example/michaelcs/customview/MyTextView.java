9e1d4aee317045dc9be86053dae6781c3bb261b8
MichealCS <1613111025@hqu.edu.cn>
2018-08-15 10:40:34 +0800

    增加CustomViewTest注释

--- CustomView/app/src/main/java/com/example/michaelcs/customview/MyTextView.java
+++ CustomView/app/src/main/java/com/example/michaelcs/customview/MyTextView.java
@@ -8,6 +8,10 @@ import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.widget.TextView;
 
+/**
+ * 这个类定义了一个自定义TextView
+ * 用类似的方法可以使得自定义控件不断重用
+ */
 public class MyTextView extends android.support.v7.widget.AppCompatTextView
 {
     Paint paint1 = new Paint();
@@ -17,6 +21,13 @@ public class MyTextView extends android.support.v7.widget.AppCompatTextView
         super(context);
     }
 
+    /**
+     * Paint是画笔：
+     * 在构造方法里面初始化画笔属性
+     *
+     * @param context
+     * @param attrs
+     */
     public MyTextView(Context context, @Nullable AttributeSet attrs)
     {
         super(context, attrs);
@@ -26,9 +37,15 @@ public class MyTextView extends android.support.v7.widget.AppCompatTextView
         paint2.setStyle(Paint.Style.FILL);
     }
 
+    /**
+     * 可以在super.onDraw(canvas)之前，即回调父类方法之前实现自己的逻辑
+     * 也可以在回调父类方法之后实现自己的逻辑
+     * @param canvas canvas是一个画板
+     */
     @Override
     protected void onDraw(Canvas canvas)
     {
+        //绘制外层矩形
         canvas.drawRect(
                 0,
                 0,
@@ -36,6 +53,7 @@ public class MyTextView extends android.support.v7.widget.AppCompatTextView
                 getMeasuredHeight(),
                 paint1
         );
+        //绘制内层矩形
         canvas.drawRect(
                 10,
                 10,
@@ -43,7 +61,9 @@ public class MyTextView extends android.support.v7.widget.AppCompatTextView
                 getMeasuredHeight()-10,
                 paint2
         );
+        //确认完成绘制
         canvas.save();
+        //绘制文字前平移十个像素
         canvas.translate(10,0);
         super.onDraw(canvas);
         canvas.restore();
